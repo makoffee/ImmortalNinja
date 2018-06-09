230c1e6403642936db17ba91d0d52c7224a2361f
makoffee <makoffee@Macbook-Pro-II.local>
2018-06-09 16:40:17 +0200

    Update to UE 4.17.2, and extended touch HUD for attack

---
+++ Source/ImmortalNinja/ImmortalNinja.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ImmortalNinja.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ImmortalNinja, "ImmortalNinja" );
